8fe7ca9583a68bc68516cbea3f206707dc8b492a
Pat Brown <mrpatbrown@gmail.com>
2022-04-09 14:58:10 +1000

    Stub FormatStyle for linux

--- Sources/XcresultIssues/Diagnostic+Formatting.swift
+++ Sources/XcresultIssues/Diagnostic+Formatting.swift
@@ -1,9 +1,14 @@
 // Copyright 2022 Itty Bitty Apps Pty Ltd. See LICENSE file.
 
-import Foundation
+// NB: Stubbing this for now as it's not supported on linux
+protocol FormatStyle: Decodable, Encodable, Hashable {
+  associatedtype FormatInput
+  associatedtype FormatOutput
+  func format(_ value: Self.FormatInput) -> Self.FormatOutput
+}
 
 extension Diagnostic {
-    func formatted<F>(_ format: F) -> F.FormatOutput where F: FormatStyle, F.FormatInput == Self {
-        format.format(self)
-    }
+  func formatted<F>(_ format: F) -> F.FormatOutput where F: FormatStyle, F.FormatInput == Self {
+    format.format(self)
+  }
 }
--- Sources/XcresultIssues/ReviewDogJSON.swift
+++ Sources/XcresultIssues/ReviewDogJSON.swift
@@ -3,33 +3,33 @@
 import Foundation
 
 struct DiagnosticResult: Codable, Hashable {
-    var diagnostics: [Diagnostic]
+  var diagnostics: [Diagnostic]
 }
 
 struct Diagnostic: Codable, Hashable {
-    struct Position: Codable, Hashable {
-        var line: Int
-        var column: Int
-    }
+  struct Position: Codable, Hashable {
+    var line: Int
+    var column: Int
+  }
 
-    struct Range: Codable, Hashable {
-        var start: Position
-        var end: Position?
-    }
+  struct Range: Codable, Hashable {
+    var start: Position
+    var end: Position?
+  }
 
-    struct Location: Codable, Hashable {
-        var path: String
-        var range: Range
-    }
+  struct Location: Codable, Hashable {
+    var path: String
+    var range: Range
+  }
 
-    enum Severity: String, Codable, Hashable {
-        case unknown = "UNKNOWN_SEVERITY"
-        case error = "ERROR"
-        case info = "INFO"
-        case warning = "WARNING"
-    }
+  enum Severity: String, Codable, Hashable {
+    case unknown = "UNKNOWN_SEVERITY"
+    case error = "ERROR"
+    case info = "INFO"
+    case warning = "WARNING"
+  }
 
-    var message: String
-    var location: Location
-    var severity: Severity
+  var message: String
+  var location: Location
+  var severity: Severity
 }
